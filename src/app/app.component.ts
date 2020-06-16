1b51f6a11f2bf7ccef7499bc904795ebae7b5579
Michael Wen <rsshbos1320@gmail.com>
2020-06-16 09:27:54 +0800

    259. Outputting Posts(Bug Fixed)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,16 +37,16 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
-    this.http.get<{ [key: string]: Post}>('https://ng-complete-guide-4b9d4.firebaseio.com/posts.json')
-      . pipe(map(responseData => {
+    this.http.get<{ [key: string]: Post }>('https://ng-complete-guide-4b9d4.firebaseio.com/posts.json')
+      .pipe(map(responseData => {
         const postsArray: Post[] = [];
         for (const key in responseData) {
           if (responseData.hasOwnProperty(key)) {
             postsArray.push({...responseData[key], id: key});
           }
-          return postsArray;
         }
-    }))
+        return postsArray;
+      }))
       .subscribe(posts => {
         this.loadedPosts = posts;
       });
